64c8b3626ef11c95b021ace425a4ddf793337276
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-02-08 22:11:53 -0800

    still suck on the loop but i think i have figured out

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -5,8 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("1. Start/Continue Routine");
+
             Console.WriteLine("2. View Progess");
+
             Console.WriteLine("3. Exit");
+
+            Console.Write("\nEnter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            while (choice != 3)
+            {
+                Console.WriteLine("Rountine step " + choice + " completed.");
+                choice++;
+
+                Console.Write("\nEnter choice: ");
+                
+
+            }
+
+
+
+
         }
     }
 }
